2bafd30dc651a2e3d2d45282aff87960ec7162b1
Amar0658 <137255721+Amar0658@users.noreply.github.com>
2024-02-06 15:28:59 +0530

    Update Fibonacci_using_recursion.c

--- Fibonacci_using_recursion.c
+++ Fibonacci_using_recursion.c
@@ -1,4 +1,4 @@
-// Online C compiler to run C program online
+// print n Fibonacci numbers
 #include <stdio.h>
 fibonacci(int n)
 {
@@ -6,6 +6,33 @@ fibonacci(int n)
     printf("%d ",a);
     if(n<3)
     return;
+    sum=a+b;
+    a=b;
+    b=sum;
+    fibonacci(n-1);
+}
+int main() {
+    // Write C code here
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if(n==1)
+    printf("%d",0);
+    else
+    {printf("0 ");
+    fibonacci(n);}
+    return 0;
+}
+
+
+//Fibonacci numbers less than n
+#include <stdio.h>
+fibonacci(int n)
+{
+    static int a=1,b=1,sum;
+    if(a>n)
+    return;
+    printf("%d ",a);
     sum=a+b;
     a=b;
     b=sum;
